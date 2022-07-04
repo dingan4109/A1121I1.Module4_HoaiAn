ef5865af72b13892dc64dd071ab17bcb543e4e27
dingan4109 <94068582+dingan4109@users.noreply.github.com>
2022-07-04 20:55:00 +0700

    ss5

---
+++ ss5_database_ORM/bai tap/music_app/src/main/java/music_app/service/MusicServiceImpl.java
@@ -0,0 +1,50 @@
+package music_app.service;
+
+import music_app.entity.Music;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
+@Transactional
+public class MusicServiceImpl implements MusicService {
+    @PersistenceContext
+    public EntityManager entityManager;
+
+    @Override
+    public List<Music> getAll() {
+        String strQuery = "SELECT m FROM Music AS m";
+        return entityManager.createQuery(strQuery,Music.class).getResultList();
+    }
+
+    @Override
+    public Music findById(int id) {
+        return entityManager.find(Music.class,id);
+    }
+
+    @Override
+    public Music findByName(String name) {
+        return null;
+    }
+
+    @Override
+    public void create(Music music) {
+        entityManager.persist(music);
+    }
+
+    @Override
+    public void update(Music music) {
+        entityManager.merge(music);
+    }
+
+    @Override
+    public void delete(int id) {
+        Music music = findById(id);
+        entityManager.remove(music);
+    }
+}
---
+++ ss5_database_ORM/bai tap/music_app/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HP
+  Date: 7/3/2022
+  Time: 10:27 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
